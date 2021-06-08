3543b65552401448f24318c106b3583524d11a83
TomasLAbreu <tomasld.abreu@gmail.com>
2021-06-08 19:12:14 +0100

    FSM

--- 04Code/FSM/Core/Inc/fsm.h
+++ 04Code/FSM/Core/Inc/fsm.h
@@ -9,14 +9,15 @@
 /******************************************************************************
 FSM states
 ******************************************************************************/
-#define S_STOPPED		0
-#define S_RECEIVE		1
-#define S_FLW_LINE 		2
-#define S_ROOM			3
-#define S_RD_RFID		4
-#define S_ROTATE		5
-#define S_WAIT			6
-#define S_ERROR			7
+#define S_STOPPED		0	// Robot stopped. Waiting for interaction
+#define S_RECEIVE		1	// Robot is receiving a new route
+
+#define S_FLW_LINE 		2	// Robot is following line
+#define S_RD_RFID		4	// Robot detects and reads RFID
+#define S_NEXT_MOV		5	// Robot determines next movement to make
+#define S_ROTATE		6	// Robot rotates in order to move in other direction
+
+#define S_ERROR			7	// Robot waiting for intervention
 
 /******************************************************************************
 FSM current state pointer
