47c1fa018ca4f19824a9a6dc7ce498052d89de06
TomasLAbreu <tomasld.abreu@gmail.com>
2021-06-19 16:59:09 +0100

    Implementacao em SW

--- 04Code/FSM/Core/Inc/route.h
+++ 04Code/FSM/Core/Inc/route.h
@@ -13,13 +13,14 @@ Route Definition
 ******************************************************************************/
 // action enum - defines the next move when founds a cross
 typedef enum{
-	ACT_RIGHT,		// turns right (cross)
-	ACT_LEFT,		// turns left (cross)
-	ACT_FORWARD,		// don't stop (room)
-	ACT_STOP			// stops (room)
+	ACT_RIGHT,		// turn right (at a cross)
+	ACT_LEFT,		// turn left (at a cross)
+	ACT_FORWARD,	// don't stop (at a room)
+	ACT_STOP		// stops (at a room)
 } action_e;
 
 // checkpoint struct definition
+
 typedef struct{
 	char *RFID;			// RFID: checkpoint unique identifier
 	action_e action;	// Action to execute at the checkpoint
@@ -27,6 +28,5 @@ typedef struct{
 
 // route_t is a series of checkpoints
 typedef checkpoint_t* route_t;
-
 
 #endif /* _ROUTE_H_ */
